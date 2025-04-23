026e1b1e52c8075bf73a9c890847d85ee12a8fed
Felipe Barreto <pipe.barreto07@gmail.com>
2025-04-23 16:10:00 -0500

    more network problems

--- infra/main.tf
+++ infra/main.tf
@@ -71,6 +71,18 @@ resource "azurerm_network_security_group" "taller_nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+
+  security_rule {
+  name                       = "sonarqube"
+  priority                   = 200
+  direction                  = "Inbound"
+  access                     = "Allow"
+  protocol                   = "Tcp"
+  source_port_range          = "*"
+  destination_port_range     = "9000"
+  source_address_prefix      = "*"
+  destination_address_prefix = "*"
+}
 }
 
 # Create network interface
@@ -152,6 +164,11 @@ resource "azurerm_public_ip" "sonar_public_ip" {
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Static"
   sku                 = "Standard"
+}
+
+resource "azurerm_network_interface_security_group_association" "sonar_nic_association" {
+  network_interface_id      = azurerm_network_interface.sonar_nic.id
+  network_security_group_id = azurerm_network_security_group.taller_nsg.id
 }
 
 resource "azurerm_network_interface" "sonar_nic" {
