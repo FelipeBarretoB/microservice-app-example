e7e0af49da02fc26fdf90578725470611f3d08d4
Felipe Barreto <pipe.barreto07@gmail.com>
2025-04-23 15:39:12 -0500

    problems with the nic

--- infra/main.tf
+++ infra/main.tf
@@ -145,11 +145,24 @@ resource "azurerm_linux_virtual_machine" "my_terraform_vm" {
 
 }
 
+resource "azurerm_network_interface" "sonar_nic" {
+  name                = "sonarNIC"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "sonar_nic_configuration"
+    subnet_id                     = azurerm_subnet.taller_subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.taller_public_ip.id
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "my_terraform_vm_sonar" {
   name                  = "Sonar-vm"
   location              = azurerm_resource_group.rg.location
   resource_group_name   = azurerm_resource_group.rg.name
-  network_interface_ids = [azurerm_network_interface.taller_nic.id]
+  network_interface_ids = [azurerm_network_interface.sonar_nic.id]
   size                  = "Standard_DS1_v2"
 
   os_disk {
