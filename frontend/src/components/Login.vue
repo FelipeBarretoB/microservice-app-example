e698f8c5eb3b1cb4c25faf44f00ca0fecd2ca240
Felipe Barreto <pipe.barreto07@gmail.com>
2025-04-23 20:14:28 -0500

    falto una cosa

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -7,7 +7,7 @@
                 <div class='row'>
                     <div class='col-md-3'></div>
                     <div class='col-md-6'>
-                        <h2>Prueba changes3 Login</h2>
+                        <h2>Prueba changes Login</h2>
                         <hr>
                     </div>
                 </div>
